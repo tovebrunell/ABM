7fb818ef7d845b633acf00266d8b452eadb3652e
Tove Brunell <tbrunell@ug.kth.se>
2025-11-24 15:22:27 +0100

    Re tal

--- Agenter.py
+++ Agenter.py
@@ -1,15 +1,18 @@
-from Modell import SIRModel
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 import random
 
-class SIRAgent(Agent):
+class SIRAgent(Agent): 
+    """ Docstring: 
+
+    """ 
     def __init__(self, unique_id, model, status="S", vaccinated=False):
         super().__init__(model)  # bara model!
         self.unique_id = unique_id  # sätt unik ID själv
         self.status = status        # "S", "I", "R", "D"
         self.vaccinated = vaccinated
         self.days_infected = 0
+        self.infector_id = None # Hur många personer den har smittat
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
@@ -21,15 +24,18 @@ class SIRAgent(Agent):
         self.model.grid.move_agent(self, new_position) # uppdaterar position
 
     def try_infect(self, other):
-        if other.status == "S":
-            if other.vaccinated:
-                # Vaccinerade har 5% risk att bli smittade
-                infection_chance = 0.03
+        if other.status == "S" or other.status == "R": 
+            if other.status == "R":
+                # Vaccinerade har 3% risk att bli smittade
+                infection_chance = 0.03 * 0.19 # Ska vi ta gånger beta? 
             else:
-                infection_chance = 1.0
+                infection_chance = 1.0 * 0.19 # Ska vi ta gånger beta? 
                 
             if self.random.random() < infection_chance:
                 other.status = "I" ## Detta leder till att alla som är icke vaccinerade blir sjuka, detta behöver vi ändra
+                other.infector_id = self.unique_id # logga vem som smittade
+                self.model.log_infection(other) # lägg till i modellens logg 
+                
 
     def step(self):
         if self.status == "D":
@@ -52,6 +58,6 @@ class SIRAgent(Agent):
                 self.status = "D"
                 return
 
-            # Återhämtning efter 10 dagar
-            if self.days_infected >= 10:
-                self.status = "R"
+            # Återhämtning efter 8 dagar
+            if self.days_infected >= 8:
+                self.status = "R" # Vi antar att man inte kan bli sjuk 2 gånger! 
