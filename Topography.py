73d9e6a3f6223fb8429aea08d85b2d80fbc308c8
Tobias Wingqvist <tobias.wingvqist04@gmail.com>
2025-12-30 13:15:20 +0100

    Removed superfluous topography and chat.gpt SIR files

--- Topography.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# Läs in kalkylarket som beskriver topografin
-df = pd.read_excel("Topografi karta.xlsx", header=None)
-
-# Konvertera direkt till en NumPy matrix
-matrix = df.to_numpy()
